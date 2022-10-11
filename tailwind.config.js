abb957e0034fe834957dbe535d0dc3b9da51231d
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-10-11 20:01:53 +0800

    Update default breakpoint media-query

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    screens: {
+      'mobile'  : {'max' : '375px'},       // => @media (max-width: 375px) { ... }
+      'tablet'  : {'max' : '640px'},       // => @media (max-width: 640px) { ... }
+      'laptop'  : {'max' : '1024px'},      // => @media (max-width: 1024px) { ... }
+      'desktop' : '1440px',                // => @media (min-width: 1440px) { ... }
+    },
     fontFamily: {
       sans      : ['Nunito Sans', 'sans-serif'],
       serif     : ['serif'],
