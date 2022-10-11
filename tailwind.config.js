e335b3f8958820decdf5cef97e1854bc8c39236e
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-10-11 20:01:35 +0800

    Add google-font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    fontFamily: {
+      sans      : ['Nunito Sans', 'sans-serif'],
+      serif     : ['serif'],
+    },
     extend: {},
   },
   plugins: [],
