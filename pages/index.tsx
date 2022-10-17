0706558a47a9e7523af8091174a19e66e5e56722
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-10-17 11:37:25 +0800

    Create Layout wrapper component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,17 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
 
 const Home: NextPage = () => {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
-  )
-}
+    return (
+        <>
+            <Head>
+              <title>Our World, Our Earth</title>
+            </Head>
+            <div className="min-h-[92vh] p-6">
+                Main Content
+            </div>
+        </>
+    );
+};
 
-export default Home
+export default Home;
