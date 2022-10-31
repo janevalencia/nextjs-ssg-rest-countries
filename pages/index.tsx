71c1e576b1d59a2253eb0d70f6407efaedd94630
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-10-31 22:52:03 +1100

    Fetch from country API using getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,40 @@
-import type { NextPage } from "next";
+import type { GetStaticProps, InferGetStaticPropsType } from "next";
+import axios from 'axios';
 import Head from "next/head";
 
-const Home: NextPage = () => {
+const Home = ({
+    countries,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
     return (
         <>
             <Head>
-              <title>Our World, Our Earth</title>
+                <title>Our World, Our Earth</title>
             </Head>
             <div className="min-h-screen w-full p-6">
-                Main Content
+                <ul>
+                    {countries.map( (country, index) => (
+                        <li key={index}>{country.name}</li>
+                    ) )}
+                </ul>
             </div>
         </>
     );
+};
+
+export const getStaticProps: GetStaticProps<{
+    countries: any[];
+}> = async () => {
+
+    // Fetch data from countries API.
+    const res = await axios.get("https://restcountries.com/v2/all");
+    const countries = await res.data;
+
+    // Return the static props.
+    return {
+        props: {
+            countries,
+        },
+    };
 };
 
 export default Home;
