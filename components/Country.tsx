bc5ca4a1a2de05b3a2289625b89d1ce934119ec3
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-11-14 17:55:38 +0800

    Add hover effect to country card component

--- components/Country.tsx
+++ components/Country.tsx
@@ -9,7 +9,7 @@ type CountryProps = {
 // Render Country component.
 const Country = ({ country }: { country: TCountry }) => {
     return (
-        <div className="bg-white shadow-md rounded-md">
+        <div className="bg-white shadow-md rounded-md hover:cursor-pointer hover:scale-105 duration-300">
             <div className="mb-4">
                 <picture>
                     <source srcSet={country.flags.png} type="image/png" />
