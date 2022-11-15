a103d59eaa4502af61fb46b4527ca68617fdb71d
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-11-15 10:04:05 +0800

    Create the search bar component on index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import type { GetStaticProps, InferGetStaticPropsType } from "next";
 import Head from "next/head";
+import api from "../api";
 import { TCountry } from "../types";
-import axios from "axios";
 import { Country } from "../components";
+import { GrSearch } from "react-icons/gr";
 
 const Home = ({
     countries,
@@ -12,13 +13,24 @@ const Home = ({
             <Head>
                 <title>Our World, Our Earth</title>
             </Head>
-            <div className="min-h-screen bg-lt-mode-bg w-full p-6">
-                <div className="grid grid-cols-1 px-10 py-2 tablet:px-0 tablet:grid-cols-2 laptop:grid-cols-3 desktop:grid-cols-4 gap-10">
-                    {countries.map((item, index) => (
-                        <Country
-                            key={index}
-                            country={item}
+            <div className="min-h-screen bg-lt-mode-bg w-full px-6">
+                {/* Toolbar */}
+                <div className="py-4">
+                    {/* Search Bar */}
+                    <div className="flex flex-row justify-between items-center gap-4 px-4 py-2 tablet:w-[500px] bg-white rounded-md shadow-md">
+                        <GrSearch size={18} className="text-gray-600" />
+                        <input
+                            className="text-lt-mode-text w-full p-2 bg-transparent focus:outline-none"
+                            type="text"
+                            placeholder="Search for a country..."
                         />
+                    </div>
+                </div>
+
+                {/* Country List */}
+                <div className="grid grid-cols-1 px-10 py-4 tablet:px-0 tablet:grid-cols-2 laptop:grid-cols-3 desktop:grid-cols-4 gap-10">
+                    {countries.map((item, index) => (
+                        <Country key={index} country={item} />
                     ))}
                 </div>
             </div>
@@ -30,7 +42,7 @@ export const getStaticProps: GetStaticProps<{
     countries: TCountry[];
 }> = async () => {
     // Fetch data from countries API.
-    const res = await axios.get("https://restcountries.com/v2/all");
+    const res = await api.get("all");
     const countries = await res.data;
 
     // Return the static props.
