6add7c25c486669a3ce1c526473ca2ad4ccd3b8c
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-11-17 13:53:36 +0800

    Create a functioning Filter component wrapped with Portal

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,34 +1,88 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { RiArrowDropDownLine } from "react-icons/ri";
+import Portal from "./Portal";
 
+// Define the default props value.
 const defaultFilterProps = {
-    defaultFilter : "Filter by Region"
-}
+    defaultFilter: "Filter by Region",
+};
 
+// Define props of Filters component.
 type FiltersProps = {
-    defaultFilter : string
+    defaultFilter: string;
 } & typeof defaultFilterProps;
 
-const Filters = ({defaultFilter} : FiltersProps ) => {
-    const [filter, setFilter] = useState<string>(defaultFilter);
+// Render component.
+const Filters = ({ defaultFilter }: FiltersProps) => {
+    const [activeFilter, setActiveFilter] = useState<string>(defaultFilter);
+
+    // Manage state opening and disclosing filter options.
+    const [openFilters, setOpenFilters] = useState<boolean>(false);
+
+    const handleFilter = (value : string) => {
+        // Set the current active filter state.
+        setActiveFilter(value);
+
+        // Disclose filter options.
+        setOpenFilters(false);
+    }
 
     return (
-        <div className="flex flex-col gap-2 w-[200px]">
+        <div className="relative w-[200px]">
             <div className="py-2 w-full bg-white rounded-md shadow-md">
-                <button className="flex flex-row justify-between items-center gap-4 w-full">
-                    {filter}
+                <button
+                    className="flex flex-row justify-between items-center gap-4 w-full"
+                    onClick={() => setOpenFilters(!openFilters)}
+                >
+                    {activeFilter}
                     <RiArrowDropDownLine size={20} />
                 </button>
             </div>
-            <div className="p-5 w-full bg-white rounded-md shadow-md">
-                <ul className="flex flex-col gap-2">
-                    <li className="cursor-pointer hover:underline" onClick={() => setFilter("Africa")}>Africa</li>
-                    <li className="cursor-pointer hover:underline" onClick={() => setFilter("America")}>America</li>
-                    <li className="cursor-pointer hover:underline" onClick={() => setFilter("Asia")}>Asia</li>
-                    <li className="cursor-pointer hover:underline" onClick={() => setFilter("Europe")}>Europe</li>
-                    <li className="cursor-pointer hover:underline" onClick={() => setFilter("Oceania")}>Oceania</li>
-                    <li className="cursor-pointer hover:underline" onClick={() => setFilter(defaultFilter)}>Show All</li>
-                </ul>
+            <div id="country_filter_options" className="absolute top-[110%] w-full">
+                {openFilters && (
+                    <Portal wrapperId="country_filter_options">
+                        <div className="p-5 w-full bg-white rounded-md shadow-md">
+                            <ul className="flex flex-col gap-2">
+                                <li
+                                    className="cursor-pointer hover:underline"
+                                    onClick={() => handleFilter("Africa")}
+                                >
+                                    Africa
+                                </li>
+                                <li
+                                    className="cursor-pointer hover:underline"
+                                    onClick={() => handleFilter("America")}
+                                >
+                                    America
+                                </li>
+                                <li
+                                    className="cursor-pointer hover:underline"
+                                    onClick={() => handleFilter("Asia")}
+                                >
+                                    Asia
+                                </li>
+                                <li
+                                    className="cursor-pointer hover:underline"
+                                    onClick={() => handleFilter("Europe")}
+                                >
+                                    Europe
+                                </li>
+                                <li
+                                    className="cursor-pointer hover:underline"
+                                    onClick={() => handleFilter("Oceania")}
+                                >
+                                    Oceania
+                                </li>
+                                <li
+                                    className="cursor-pointer hover:underline"
+                                    onClick={() => handleFilter(defaultFilter)}
+                                >
+                                    Show All
+                                </li>
+                            </ul>
+                        </div>
+                    </Portal>
+                )}
             </div>
         </div>
     );
