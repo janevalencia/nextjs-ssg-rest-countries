65a6f4e1888362cb028ec8737a5ecff3e0f8436a
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-12-03 15:36:42 +0800

    Setup context to Header theme-toggler

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,31 @@
 import { BsFillMoonFill } from "react-icons/bs";
+import { useContext } from "react";
+import ThemeContext from "../store/ThemeContext";
 
 const Header = () => {
+    // Set context.
+    const themeContext: {
+        isDarkMode: boolean;
+        toggleThemeHandler: () => void;
+    } = useContext(ThemeContext);
+
+    // Render.
     return (
         <header className="flex flex-row flex-wrap justify-between items-center border-b-2 border-gray-100 w-full min-h-[8vh] px-16 py-6">
-            <h1 className="text-lg py-2 tablet:text-xl desktop:text-2xl">Where in the world?</h1>
-            <div className="flex justify-end items-center gap-2 cursor-pointer text-base desktop:text-lg">
-                <BsFillMoonFill className="mx-1 my-2" />
-                <p className="text-bold"><span className="underline text-gray-600 hover:text-gray-800 duration-500">Dark</span> Mode</p>
-            </div>
+            <h1 className="text-lg py-2 tablet:text-xl desktop:text-2xl">
+                Where in the world?
+            </h1>
+            <button className="p-0" onClick={() => {themeContext.toggleThemeHandler()}}>
+                <div className="flex justify-end items-center gap-2 cursor-pointer text-base desktop:text-lg">
+                    <BsFillMoonFill className="mx-1 my-2" />
+                    <p className="text-bold">
+                        <span className="underline text-gray-600 hover:text-gray-800 duration-500">
+                            {!themeContext.isDarkMode ? "Dark" : "Light"}
+                        </span>{" "}
+                        Mode
+                    </p>
+                </div>
+            </button>
         </header>
     );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "../components";
+import { ThemeContextProvider } from "../store/ThemeContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+        <ThemeContextProvider>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </ThemeContextProvider>
     );
 }
 
