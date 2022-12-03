25c9f74bf2571550bdba2111d162f84683712d81
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-12-03 17:15:55 +0800

    Update styling on 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -10,7 +10,7 @@ const NotFound : NextPage = () => {
                 <title>Ooops! Page Not Found.</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <div className='page-container w-full px-6 py-4 h-[65vh] bg-lt-mode-bg'>
+            <div className='page-container w-full px-16 py-4 min-h-[70vh] bg-lt-mode-bg flex flex-col justify-center'>
                 <h1 className='text-2xl'>🦄 Ooops ... 404! Page Not Found.</h1>
                 <p className='text-lt-mode-text leading-10'>Click here to return to <Link href='/'><a className='text-blue-600'>homepage</a></Link>.</p>
             </div>
